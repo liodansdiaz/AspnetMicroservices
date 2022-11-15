37bd0f8532960d709315acb1e71d836b5db7a83d
liodansdiaz <liodansdiaz@gmail.com>
2022-11-15 02:20:55 -0500

    catalogAPI(1ra)

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabasesSetting:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabasesSetting:ConnectionString"));
+            Product = database.GetCollection<Product>(configuration.GetValue<string>("DatabasesSetting:CollectionName);
+        }
+
+        public IMongoCollection<Product> Product { get; }
+    }
+}
